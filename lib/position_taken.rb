88111d55cc69c3d55efaeecf1abe4be374a8766c
Tiffany <ong.tifffany1503@gmail.com>
2018-09-03 20:49:38 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 
-board = ["X", "", " ", " ", " ", " ", " ", " ", "O"]
+board = [" ", "", " ", " ", " ", " ", " ", " ", " "]
 
 def position_taken?(board, position)
   taken = nil
