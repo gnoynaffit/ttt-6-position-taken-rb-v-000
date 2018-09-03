e9d7dd99bf9d130c7c5e9a9d53f4d102bbd37f25
Tiffany <ong.tifffany1503@gmail.com>
2018-09-03 20:23:55 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,3 +5,8 @@ def postion_taken? (board, index)
   ! (board[index] ==  " " || board
   [index] == "" || board[index] == nil)
    end
+   puts position_taken?(ttt_board, 0)
+   puts position_taken?(ttt_board, 1)
+   puts position_taken?(ttt_board, 2)
+   puts position_taken?(ttt_board, 3)
+   puts position_taken?(ttt_board, 4)
