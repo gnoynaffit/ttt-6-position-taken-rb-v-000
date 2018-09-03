9ba62ec697112ec7ae637f84b159430abade370c
Tiffany <ong.tifffany1503@gmail.com>
2018-09-03 20:17:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,15 @@
 # code your #position_taken? method here!
 def postion_taken? (board, index)
-  
+   taken = nil
+   if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+     taken = false
+   else
+     taken = true
+   end
+   taken
+
+puts position_taken?(ttt_board, 0)
+puts position_taken?(ttt_board, 1)
+puts position_taken?(ttt_board, 2)
+puts position_taken?(ttt_board, 3)
+puts position_taken?(ttt_board, 4)
