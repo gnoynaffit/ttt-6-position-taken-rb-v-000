f9316ef0a999bc261d4d169f6528ca28c2284e2d
Tiffany <ong.tifffany1503@gmail.com>
2018-09-03 21:06:37 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,8 @@
 # code your #position_taken? method here!
-
-
-position_taken?(board, index)
-  position = board[index]
+position = board[index]
   if position == " " || position == "" || position == nil
-    false
-end 
+
+
 
 
 
