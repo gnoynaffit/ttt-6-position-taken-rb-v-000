ac7402bbd47d8259cb094ee2cc6b5b7b441f3fc9
Tiffany <ong.tifffany1503@gmail.com>
2018-09-03 20:22:51 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,6 @@
 # code your #position_taken? method here!
+ttt_board = ["",""," "," ","X"," "," "," ",""]
+
 def postion_taken? (board, index)
   ! (board[index] ==  " " || board
   [index] == "" || board[index] == nil)
