860ffd8212f596cb6164c578c33ce7f9b630a652
Tiffany <ong.tifffany1503@gmail.com>
2018-09-03 21:15:48 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,14 @@
 # code your #position_taken? method here!
-def position_taken?(board, position)
-  taken = nil
-  if board[position] == " "
-    taken = false
-  else
-    taken = true
-  end
-  taken
-end
+#def position_taken?(board, position)
+  #taken = nil
+  #if board[position] == " "
+    #taken = false
+  #else
+    #taken = true
+  #end
+  #taken
+#end
+
 #if position == " " || position == "" || position == nil
    #return false
  #end
